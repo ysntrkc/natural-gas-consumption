a2a6eeb8969418771d21518344c286bd6563c482
ysntrkc <yasin.trkc.142@gmail.com>
2022-08-05 12:02:04 +0300

    design about us page and main page.

--- 00_Main_Page_🏡.py
+++ 00_Main_Page_🏡.py
@@ -1,8 +1,34 @@
 # importing necessary libs
 import numpy as np
 import pandas as pd
+from pyparsing import col
 import streamlit as st
 
 st.set_page_config(page_title="Main Page", page_icon="🏡")
 
 st.title("Main Page")
+
+st.markdown(
+    """
+        ---
+        ## Description
+        ##### This project is our graduation project for K136. Istanbul Data Science Bootcamp. In this project, we used the weather and natural gas consumption data from İBB. To estimate natural gas use, we first found the monthly average temperature and humidity data for 2022 using time series. Then, using this data and historical data, we estimated the natural gas consumption in 2022 with XGBoost.
+        ---
+        ## Model Development
+        ##### We used Prophet for time series forecasting and XGBoost for regression.
+        ---
+        ## Supporters
+    """
+)
+
+col1, _, col2 = st.columns([2, 1, 2])
+with col1:
+    st.image("images/1.png")
+    st.image("images/3.png")
+with col2:
+    st.image("images/2.png")
+    st.image("images/4.png")
+
+_, col1, _ = st.columns([1.5, 2, 1.5])
+with col1:
+    st.image("images/5.png")
---
+++ pages/02_About_Us_🔥.py
@@ -0,0 +1,46 @@
+from matplotlib.pyplot import ginput
+import streamlit as st
+
+st.set_page_config(page_title="About Us", page_icon="🔥")
+
+with open("images/github.txt", "r") as f:
+    github = f.read()
+with open("images/linkedin.txt", "r") as f:
+    linkedin = f.read()
+
+
+st.title("About Us")
+st.markdown("---")
+
+_, col, _ = st.columns([1, 1, 1])
+with col:
+    st.image("images/yasin.jpg", caption="Yasin Tarakçı")
+
+_, col1, col2, _ = st.columns([3, 1.5, 0.8, 3])
+with col1:
+    st.markdown(f"[![Foo]({github})](https://github.com/ysntrkc)")
+with col2:
+    st.markdown(f"[![Foo]({linkedin})](https://www.linkedin.com/in/yasintarakci)")
+st.markdown("---")
+
+_, col, _ = st.columns([1, 1, 1])
+with col:
+    st.image("images/yasin.jpg", caption="Yasin Tarakçı")
+
+_, col1, col2, _ = st.columns([3, 1.5, 0.8, 3])
+with col1:
+    st.markdown(f"[![Foo]({github})](https://github.com/ysntrkc)")
+with col2:
+    st.markdown(f"[![Foo]({linkedin})](https://www.linkedin.com/in/yasintarakci)")
+st.markdown("---")
+
+_, col, _ = st.columns([1, 1, 1])
+with col:
+    st.image("images/yasin.jpg", caption="Yasin Tarakçı")
+
+_, col1, col2, _ = st.columns([3, 1.5, 0.8, 3])
+with col1:
+    st.markdown(f"[![Foo]({github})](https://github.com/ysntrkc)")
+with col2:
+    st.markdown(f"[![Foo]({linkedin})](https://www.linkedin.com/in/yasintarakci)")
+st.markdown("---")
